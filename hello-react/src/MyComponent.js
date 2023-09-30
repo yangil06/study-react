a26a9694afa59256537a68109c1440d066df8949
Han-Gil EE <hangil0609@gmail.com>
2023-09-30 20:45:39 +0900

    한줄 쓸 때, 브레이킷 필요 없는 것과 한줄로 작성할 때, 리턴이 필요 없는 것도 확인

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -2,8 +2,10 @@
 //   return <div>My new cool component</div>;
 // };
 
-const MyComponent = (props) => {
-  return <div>Hi, my name is {props.name}</div>;
-};
+// const MyComponent = (props) => {
+//   return <div>Hi, my name is {props.name}</div>;
+// };
+
+const MyComponent = (props) => <div>Hi, My name is {props.name}</div>;
 
 export default MyComponent;
