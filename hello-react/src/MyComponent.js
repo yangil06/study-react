48bd16f72b2ab9dc14e6e1cddbd65b074ed642fb
Han-Gil EE <hangil0609@gmail.com>
2023-09-30 20:43:03 +0900

    일단 함수 컴포넌트에, props를 넣어서 동작이 되는 것을 봤고.. props 가 괄호 없이 쓰는 방법도 있는데, 일단 괄호에 넣어봤다. 다음 커밋에 괄호 안하고 한번 넣어보겠음

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,5 +1,9 @@
-const MyComponent = () => {
-  return <div>My new cool component</div>;
+// const MyComponent = () => {
+//   return <div>My new cool component</div>;
+// };
+
+const MyComponent = (props) => {
+  return <div>Hi, my name is {props.name}</div>;
 };
 
 export default MyComponent;
