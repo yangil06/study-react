77f5df0ee41ffe99594b2e346399d3a4d192b14f
Han-Gil EE <hangil0609@gmail.com>
2023-10-03 16:52:31 +0900

    setState에 의해.. 하나의 값만 설정되는데..?

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -1,52 +1,48 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 
-class EventPractice extends Component {
-  state = {
+const EventPractice = () => {
+  const [{ username, message }, setState] = useState({
     username: '',
     message: '',
-  };
+  });
 
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+  const handleChange = (e) => {
+    setState({ [e.target.name]: e.target.value });
   };
-  handleClick = () => {
-    alert(this.state.username + ': ' + this.state.message);
-    this.setState({
+  const handleClick = () => {
+    alert(username + ': ' + message);
+    setState({
       username: '',
       message: '',
     });
   };
-  handleKeyDown = (e) => {
+  const handleKeyDown = (e) => {
     if (e.key === 'Enter') {
-      this.handleClick();
+      handleClick();
     }
   };
 
-  render() {
-    //Component를 상속받아서 render 생성자 작성해주는것인가?
-    //일단 render(): ReactNode; 이런 함수가 있긴 하네..
-    return (
-      <div>
-        <h1>이벤트 연습</h1>
-        <input
-          type="text"
-          name="username"
-          placeholder="username"
-          onChange={this.handleChange}
-        ></input>
-        <br></br>
-        <input
-          type="text"
-          name="message"
-          placeholder="type anything"
-          onChange={this.handleChange}
-          onKeyDown={this.handleKeyDown}
-        ></input>
-        <br></br>
-        <button onClick={this.handleClick}>message값 확인</button>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h1>이벤트 연습</h1>
+      <input
+        type="text"
+        name="username"
+        placeholder="username"
+        onChange={handleChange}
+      ></input>
+      <br></br>
+      <input
+        type="text"
+        name="message"
+        placeholder="type anything"
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      ></input>
+      <br></br>
+      <button onClick={handleClick}>message값 확인</button>
+    </div>
+  );
+};
 
 export default EventPractice;
