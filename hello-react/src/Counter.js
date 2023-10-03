747ade1c2f37cd974a0d4497c30454e4caedc5d1
Han-Gil EE <hangil0609@gmail.com>
2023-10-03 11:52:20 +0900

    fixedNumber added, destructing assignment

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -6,14 +6,16 @@ class Counter extends Component {
     //state 초기값 설정하기
     this.state = {
       number: 0,
+      fixedNumber: 0,
     };
   }
 
   render() {
-    const { number } = this.state;
+    const { number, fixedNumber } = this.state;
     return (
       <div>
         <h1>{number}</h1>
+        <h2>바뀌지 않는 값 : {fixedNumber}</h2>
         <button
           onClick={() => {
             this.setState({ number: number + 1 });
