7f0973ce656e925a733f79637c680641e2873723
Han-Gil EE <hangil0609@gmail.com>
2023-10-03 16:12:06 +0900

    setState 에 값만 넣는건 안된다..? 실제로는 함수를 리턴해야 되는거고, 함수로 state이라 할수 있는 객체 또는 dic을 리턴해야함

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 class EventPractice extends Component {
+  state = { message: '' };
+
   render() {
     //Component를 상속받아서 render 생성자 작성해주는것인가?
     //일단 render(): ReactNode; 이런 함수가 있긴 하네..
@@ -15,9 +17,19 @@ class EventPractice extends Component {
             {
               console.log(e); //SyntheticBaseEvent 는 이벤트가 발생시에만 값이 있고 그 후에 초기화 되서
               //이벤트 함수 실행 후 참조할 수 없다.
+
+              //this.setState(e.target.value);
+              this.setState({ message: e.target.value });
             }
           }}
         ></input>
+        <button
+          onClick={() => {
+            alert(this.state.message);
+          }}
+        >
+          message값 확인
+        </button>
       </div>
     );
   }
