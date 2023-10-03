844c716ed68819968878c4823d2e0b17fed81d0f
Han-Gil EE <hangil0609@gmail.com>
2023-10-03 12:12:01 +0900

    맨 아래 수행해서 이제 +2가 됨

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -24,8 +24,8 @@ class Counter extends Component {
         <h2>바뀌지 않는 값 : {fixedNumber}</h2>
         <button
           onClick={() => {
-            this.setState({ number: number + 2 });
             this.setState({ number: number + 1 });
+            this.setState({ number: number + 2 });
           }}
         >
           +1
