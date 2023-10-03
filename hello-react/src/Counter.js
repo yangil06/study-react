4004d400d38f3e76e4ef5fa6d0cc7b548995dc1c
Han-Gil EE <hangil0609@gmail.com>
2023-10-03 13:03:51 +0900

    템플릿 문자열 사용방법 겨우 찾았네, 모던 자바스크립트로 배우는 리액트 입문에서backquote 로 하는 법 참조

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -31,15 +31,14 @@ class Counter extends Component {
           // }}
           // prevState 이 undefined 로 초기화 되서 레퍼런스로만 오는거 같다.
           onClick={() => {
-            this.setState(
-              (prevState = () => {
-                return {
-                  number: number + 1,
-                  fixedNumber: 0,
-                }; //prevState을 리턴해주는거니까
-                //객체-Dic 형태로 리턴해준다.
-              })
-            );
+            this.setState((prevState, prop) => {
+              const { number, fixedNumber } = prevState;
+              console.log(
+                `prevState.number = ${number}, fixedNum = ${fixedNumber}`
+              );
+              console.log('prop = ' + prop);
+              return { number: prevState.number + 1 };
+            });
           }}
         >
           +1
