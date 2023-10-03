55d01db0ccabd889ffed2ad7cc13bb556cec263a
Han-Gil EE <hangil0609@gmail.com>
2023-10-03 13:32:20 +0900

    여기까지 하면 색 변경??

--- hello-react/src/Say.js
+++ hello-react/src/Say.js
@@ -5,11 +5,17 @@ const Say = () => {
   const onClickEnter = () => setMessage('hello!');
   const onClickLeave = () => setMessage('and goodbye!');
 
+  const [color, setColor] = useState('black');
+
   return (
     <div>
       <button onClick={onClickEnter}>입장</button>
       <button onClick={onClickLeave}>퇴장</button>
       <h1>{message}</h1>
+
+      <button onClick={() => setColor('red')}>메세지 빨강으로 변경</button>
+      <button onClick={() => setColor('green')}>메세지 초록으로 변경</button>
+      <button onClick={() => setColor('blue')}>메세지 파랑으로 변경</button>
     </div>
   );
 };
